2af7fd4fecc309b564bc52a29db2ce52ac21446d
Allan Marques Baptista <allan.marques@ymail.com>
2011-06-27 01:19:20 -0300

    mudanca importante nos diretorios e conf de diretorios.

---
+++ welearn/libraries/PHPCassa.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) {exit('No direct script access allowed');}
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrador
+ * Date: 20/06/11
+ * Time: 11:51
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class WL_PHPCassa {
+
+    function __construct()
+    {
+
+    }
+}
+
+/* End of file PHPCassa.php */
+/* Location: ./application/libraries/PHPCassa.php */
