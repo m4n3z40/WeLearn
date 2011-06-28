e593492a64b9f3abd42486c48b38b5254729eb54
Allan Marques Baptista <allan.marques@ymail.com>
2011-06-28 02:51:41 -0300

    configuracao alterada para adaptar ao servidor virtual do ubuntu padrao.

--- welearn/config/config.php
+++ welearn/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/welearn/';
+$config['base_url']	= 'http://www.welearn.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ $config['index_page'] = '';
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']	= 'QUERY_STRING';
+$config['uri_protocol']	= 'AUTO';
 
 /*
 |--------------------------------------------------------------------------
