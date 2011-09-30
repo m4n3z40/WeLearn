52a2d7a7cea742783830851932e1c21b842a4f5d
Allan Baptista <allan.marques@ymail.com>
2011-09-30 16:51:06 -0300

    Modulo de sugestao de curso finalizado

---
+++ welearn/helpers/paginacao_mysql_helper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: administrador
+ * Date: 30/09/11
+ * Time: 15:28
+ * To change this template use File | Settings | File Templates.
+ */
+ 
---
+++ welearn/libraries/WeLearn/Cursos/UsuarioJaVotouException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: administrador
+ * Date: 30/09/11
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class WeLearn_Cursos_UsuarioJaVotouException {
+
+}
