7b48419b40445dbc4e18624c42ff07191ab4f76e
Thiago Monteiro <dalmeidamonteiro@bol.com.br>
2012-04-29 14:42:30 -0300

    visualizar perfil e enviar convite

---
+++ js/convite.js
@@ -0,0 +1,7 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 25/04/12
+ * Time: 17:55
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ welearn/controllers/convite.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 25/04/12
+ * Time: 17:33
+ * To change this template use File | Settings | File Templates.
+ */
+class Convite
+{
+
+}
