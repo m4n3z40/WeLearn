be731329c554c13ec561dedc87db1b5075d5953d
Victor Hugo <gevigier@gevigier-Inspiron-1564.(none)>
2012-06-22 01:06:25 -0300

    traducao form-validation

---
+++ welearn/language/pt-br/form_validation_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['required']			= "O campo %s é obrigatoria.";
+$lang['isset']				= "O campo %s deve ser preenchido.";
+$lang['valid_email']		= "O campo %s deve conter um edereço de e-mail valido.";
+$lang['valid_emails']		= "O campo %s deve conter edereços de e-mail validos.";
+$lang['valid_url']			= "O campo %s deve conter uma URL valida.";
+$lang['valid_ip']			= "O campo %s deve conter um IP valido.";
+$lang['min_length']			= "O campo %s deve conter pelomenos %s caracteres.";
+$lang['max_length']			= "O campo %s não pode conter mais de %s  caracteres.";
+$lang['exact_length']		= "O campo %s deve ter exatamente %s caracteres.";
+$lang['alpha']				= "O campo %s so pode conter letras.";
+$lang['alpha_numeric']		= "O campo %s so deve conter caracteres alfanuméricos.";
+$lang['alpha_dash']			= "O campo %s so deve conter caracteres alfanuméricos, underline( _ ), e hifén( - ).";
+$lang['numeric']			= "O campo %s deve conter numeros.";
+$lang['is_numeric']			= "O campo %s so deve conter caracteres numericos.";
+$lang['integer']			= "O campo %s deve conter um número inteiro.";
+$lang['regex_match']		= "O campo %s não esta no formato correto.";
+$lang['matches']			= "O campo %s não coincide com o %s campo.";
+$lang['is_natural']			= "O campo %s so deve conter numeros positivos.";
+$lang['is_natural_no_zero']	= "O campo %s deve conter numeros maiores que ZERO.";
+$lang['decimal']			= "O campo %s deve conter numeros decimais.";
+$lang['less_than']			= "O campo %s deve conter numero menor que %s.";
+$lang['greater_than']		= "O campo %s deve conter numero maior que %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
